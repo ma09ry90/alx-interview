af248866f4f541fb4572b4bca5e0b70d344e5420
ma09ry90 <mengistumary60@gmail.com>
2024-10-26 08:43:32 +0000

    log-parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,50 @@
 #!/usr/bin/python3
+"""
+log parsing
+"""
 
-""" reads stdin line by line and computes metric"""
+import sys
+import re
 
-if __name__ == '__main__':
 
-    def printer(file_size, status):
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-        '''Print logs'''
 
-        print("File size: {:d}".format(file_size))
-        for i in sorted(status.keys()):
-            if status[i] != 0:
-                print("{}: {}".format(i, status[i]))
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
-    file_size = 0
-    status = {"200": 0, "301": 0, "400": 0, "401": 0,
-              "403": 0, "404": 0, "405": 0, "500": 0}
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
-    counter = 0
     try:
-        with open(0) as f:
-            for line in f:
-                counter += 1
-                data = line.split()
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
-                try:
-                    file_size += int(data[-1])
-                except Exception:
-                    pass
+                # File size
+                log["file_size"] += file_size
 
-                try:
-                    st = data[-2]
-                    if st in status:
-                        status[st] += 1
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
 
-                except Exception:
-                    pass
-                if counter % 10 == 0:
-                    printer(file_size, status)
-            printer(file_size, status)
-    except KeyboardInterrupt:
-        printer(file_size, status)
-        raise
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
